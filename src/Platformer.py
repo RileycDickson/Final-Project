a930a5250e0aa2f9065366a0bda627447e652443
Riley Dickson <rcd240000@utdallas.edu>
2025-05-09 15:59:29 -0500

    Added Collision with player and environment

--- src/Platformer.py
+++ src/Platformer.py
@@ -20,10 +20,6 @@ tile_size = 50
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 
-def draw_grid():
-	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
 class Player():
 	def __init__(self, x, y):
@@ -41,6 +37,8 @@ class Player():
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		self.vel_y = 0
 		self.jumped = False
 		self.direction = 0
@@ -50,7 +48,7 @@ class Player():
 		dy = 0
 		walk_cooldown = 5
 
-		#Player Movement
+		#get keypresses
 		key = pygame.key.get_pressed()
 		if key[pygame.K_SPACE] and self.jumped == False:
 			self.vel_y = -15
@@ -74,10 +72,9 @@ class Player():
 				self.image = self.images_left[self.index]
 
 
-
 		#handle animation
 		if self.counter > walk_cooldown:
-			self.counter= 0
+			self.counter = 0	
 			self.index += 1
 			if self.index >= len(self.images_right):
 				self.index = 0
@@ -89,11 +86,27 @@ class Player():
 
 		#add gravity
 		self.vel_y += 1
-		if self.vel_y >  10:
+		if self.vel_y > 10:
 			self.vel_y = 10
 		dy += self.vel_y
 
 		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
+
+
 
 
 		#update player coordinates
@@ -104,8 +117,12 @@ class Player():
 			self.rect.bottom = screen_height
 			dy = 0
 
-		# Draw player onto the screen
+		#draw player onto screen
 		screen.blit(self.image, self.rect)
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
+
+
+
 
 class World():
 	def __init__(self, data):
@@ -139,6 +156,7 @@ class World():
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
+			pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 
